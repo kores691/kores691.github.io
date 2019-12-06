a8bafe558eee99686e76d569346950a7c33e54c6
kores691 <36376882+kores691@users.noreply.github.com>
2019-12-06 15:08:31 +0200

    Add files via upload

---
+++ works/Calculator/main.js
@@ -0,0 +1,13 @@
+// Функция, которая скрывает каждое третее письмо в Gmail
+
+function hideEveryThirdMail(item) {
+    let emailList =  document.querySelectorAll(item);
+    emailList.forEach((item, i) => {
+        if ((i + 1) % 3 === 0) {
+            item.style.display = 'none';
+        }
+    })
+}
+
+
+hideEveryThirdMail('.zA');
